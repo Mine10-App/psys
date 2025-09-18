d242b8ba0974726fe024891daa770d744845bf5d
Mine <air1345@gmail.com>
2025-09-18 09:38:45 +0500

    Create users.js

---
+++ users.js
@@ -0,0 +1,4 @@
+const users = [
+  { username: "Leeli", name: "Leeli Lounge", password: "12345", rcNo: "1", WP: "Leeli" },
+  { username: "Koveli", name: "Koveli Lounge", password: "12345", rcNo: "2", WP: "Koveli" }
+];
